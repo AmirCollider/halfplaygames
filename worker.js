b1e593b2b8d61294ff321a3f27780360a9b85f7a
AmirCollider <86761819+AmirCollider@users.noreply.github.com>
2026-09-25 22:25:17 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -149,7 +149,7 @@ async function handleRequest(request) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta name="google-site-verification=jVFzRLY9Sg84p9L0ICueyUKxFwqlvSa82VIcGTkyXjI" />
+    <meta name="google-site-verification" content="uFvaRQchIco-iyGmdsNknLK7mL5Asxg47GjaOQmhf0Q" />
 
     <!-- [SEO-ENHANCEMENT] تگ‌های اصلی و حیاتی برای سئو -->
     <title>HalfPlay Games - 50% Off Steam Games | PC Game Deals & Charity</title>
